a67c5e058073d2b52c7b2eac3df3c0a94a2b0f7c
JamieMacKay04 <JMACKA206@caledonian.ac.uk>
2026-01-08 01:06:10 +0000

    Scanning Works!

--- barLog/src/Controller.java
+++ barLog/src/Controller.java
@@ -2,6 +2,8 @@ package barLog.src;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -16,6 +18,18 @@ public class Controller {
     public void setMainWindow(Stage mainWindow) {
         this.mainWindow = mainWindow; //Setter method to set the mainWindow variable
     }
+
+    @FXML private TextField txtInput;
+    @FXML private ListView<String> lstItems;
+
+    @FXML private void handleInput() {
+        String s = txtInput.getText().trim();
+            if (!s.isEmpty()) lstItems.getItems().add(s);
+            txtInput.clear();
+    }
+    
+    @FXML
+    private Label title;
 
     @FXML
     void onBtnClick(ActionEvent event) {
