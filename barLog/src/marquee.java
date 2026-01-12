d7578b618e55ca43a6162b8dd3ccf6891b7037ca
JamieMacKay04 <JMACKA206@caledonian.ac.uk>
2026-01-12 13:26:23 +0000

    Marquee...Morton...Posto base controllers added

--- barLog/src/marquee.java
+++ barLog/src/marquee.java
@@ -1,5 +1,70 @@
 package barLog.src;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class marquee {
+
+    @FXML
+    private Button btnApply;
+
+    @FXML
+    private Button btnBarLog;
+
+    @FXML
+    private Button btnGoMorton;
+
+    @FXML
+    private Button btnGoPosto;
+
+    @FXML
+    private Button btnGoTransfers;
+
+    @FXML
+    private Button btnHotelBar;
+
+    @FXML
+    private TableColumn<?, ?> colItem;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private DatePicker dateFrom;
+
+    @FXML
+    private DatePicker dateTo;
+
+    @FXML
+    private TableView<?> tvIntake;
+
+    @FXML
+    void goBarLog(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goHotelBar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goMorton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goPosto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goTransfers(ActionEvent event) {
+
+    }
 
 }
--- barLog/src/morton.java
+++ barLog/src/morton.java
@@ -1,5 +1,70 @@
 package barLog.src;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class morton {
+
+    @FXML
+    private Button btnApply;
+
+    @FXML
+    private Button btnGoBarLog;
+
+    @FXML
+    private Button btnGoMarquee;
+
+    @FXML
+    private Button btnGoMorton;
+
+    @FXML
+    private Button btnGoPosto;
+
+    @FXML
+    private Button btnGoTransfers;
+
+    @FXML
+    private TableColumn<?, ?> colItem;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private DatePicker dateFrom;
+
+    @FXML
+    private DatePicker dateTo;
+
+    @FXML
+    private TableView<?> tvIntake;
+
+    @FXML
+    void goBarLog(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goMarquee(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goMorton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goPosto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goTransfers(ActionEvent event) {
+
+    }
 
 }
