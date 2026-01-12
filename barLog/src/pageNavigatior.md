e69d72a8c0bdedf768058e8d29056564282244ad
JamieMacKay04 <JMACKA206@caledonian.ac.uk>
2026-01-12 13:34:01 +0000

    Page Navigation Functional

---
+++ barLog/src/pageNavigatior.md
@@ -0,0 +1,24 @@
+@FXML
+private void goPosto(ActionEvent e) {
+  Navigator.go(e, "posto.fxml");
+}
+
+@FXML
+private void goMarquee(ActionEvent e) {
+  Navigator.go(e, "marquee.fxml");
+}
+
+@FXML
+private void goMorton(ActionEvent e) {
+  Navigator.go(e, "morton.fxml");
+}
+
+@FXML
+private void goBarLog(ActionEvent e) {
+  Navigator.go(e, "mainScene.fxml");
+}
+
+@FXML
+private void goTransfers(ActionEvent e) {
+  Navigator.go(e, "transfers.fxml");
+}
--- barLog/src/posto.java
+++ barLog/src/posto.java
@@ -42,29 +42,34 @@ public class posto {
     @FXML
     private TableView<?> tvIntake;
 
-    @FXML
-    void goBarLog(ActionEvent event) {
+@FXML
+private void goPosto(ActionEvent e) {
+  Navigator.go(e, "posto.fxml");
+}
 
-    }
+@FXML
+private void goMarquee(ActionEvent e) {
+  Navigator.go(e, "marquee.fxml");
+}
 
-    @FXML
-    void goHotelBar(ActionEvent event) {
+@FXML
+private void goMorton(ActionEvent e) {
+  Navigator.go(e, "morton.fxml");
+}
 
-    }
+@FXML
+private void goBarLog(ActionEvent e) {
+  Navigator.go(e, "mainScene.fxml");
+}
 
-    @FXML
-    void goMarquee(ActionEvent event) {
+@FXML
+private void goTransfers(ActionEvent e) {
+  Navigator.go(e, "transfers.fxml");
+}
 
-    }
-
-    @FXML
-    void goMorton(ActionEvent event) {
-
-    }
-
-    @FXML
-    void goTransfers(ActionEvent event) {
-
-    }
+@FXML
+private void goHotelBar(ActionEvent e) {
+  Navigator.go(e, "hotelBar.fxml");
+}
 
 }
